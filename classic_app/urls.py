00bf99c74f2ff31c13c99f4b21c59199d970d7f3
surajrajbhar9 <surajrajbhar94@gmail.com>
2020-02-18 00:14:11 +0530

    added bootsrap in product catalog

--- classic_app/urls.py
+++ classic_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.shortcuts import render
 from .views import index , getProducts
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('',index),
-    path('getProducts',getProducts)
+    path('getProducts',getProducts),
+    path('prodcut_details',TemplateView.as_view(template_name='product_catalog.html'))
 ]
